62fb1e040ebd85feb8275d050d4f215e348437e4
VALERI Yoann <yoann.valeri@cea.fr>
2022-09-26 16:50:45 +0200

    filter: modify expiration filters to check 2 attributes
    Divide the "expired-at" filter in two, "expired-at" which
    takes in an epoch and "expired" which doesn't. "expired"
    behaves like "expired-at" with "-$(date +%s)".
    
    They both check the given value against two attributes.
    
    Change-Id: I7b62d8100ebe5bc1e26d3cecbd3f20422843aa3c
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- rbh-find-lustre.c
+++ rbh-find-lustre.c
@@ -44,7 +44,13 @@ lustre_predicate_or_action(const char *string)
     case '-':
         switch (string[1]) {
         case 'e':
-            if (!strcmp(&string[2], "xpired-at"))
+            if (strncmp(&string[2], "xpired", strlen("xpired")) != 0)
+                break;
+
+            if (string[strlen("-expired")] == 0)
+                return CLT_PREDICATE;
+
+            if (strcmp(&string[strlen("-expired")], "-at") == 0)
                 return CLT_PREDICATE;
             break;
         case 'f':
@@ -75,7 +81,7 @@ lustre_parse_predicate(struct find_context *ctx, int *arg_idx)
 
     predicate = str2lustre_predicate(ctx->argv[i]);
 
-    if (i + 1 >= ctx->argc)
+    if (predicate != LPRED_EXPIRED && i + 1 >= ctx->argc)
         error(EX_USAGE, 0, "missing argument to `%s'", ctx->argv[i]);
 
     /* In the following block, functions should call error() themselves rather
@@ -85,6 +91,9 @@ lustre_parse_predicate(struct find_context *ctx, int *arg_idx)
      * precise and meaningul error messages.
      */
     switch (predicate) {
+    case LPRED_EXPIRED:
+        filter = expired2filter();
+        break;
     case LPRED_EXPIRED_AT:
         filter = expired_at2filter(ctx->argv[++i]);
         break;
